130060f876e299d463697816e9b7d5de29093026
mdallen5393 <allen_matthew@outlook.com>
2022-11-30 10:01:39 -0800

    Task 0. Gather data from an API

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+import json
+import requests
+import sys
+
+num_done, num_tasks = 0, 0
+userId = sys.argv[1]
+
+# create Response object for specific user and that user's tasks
+user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(userId))
+todo_response = requests \
+    .get('https://jsonplaceholder.typicode.com/todos/?userId={}'
+         .format(userId))
+
+# create Dictionary objects from response objects
+user_info = json.loads(user_response.text)
+todo_info = json.loads(todo_response.text)
+
+employee_name = user_info['name']
+
+for task in todo_info:
+    num_tasks += 1
+    if task['completed']:
+        num_done += 1
+
+print("Employee {} is done with tasks({}/{}):"
+      .format(employee_name, num_done, num_tasks))
+
+for task in todo_info:
+    print(f"\t {task['title']}")
