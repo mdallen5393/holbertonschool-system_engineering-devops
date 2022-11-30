ad0b584e82eb2ad269481f9ef4901bd95697662b
mdallen5393 <allen_matthew@outlook.com>
2022-11-30 12:32:38 -0800

    Task 3. Dictionary of list of dictionaries

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that exports information about an employees TODO list progress
+given an employee id to a CSV file named `USER_ID.csv`.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    user_response = requests.get(url)
+    user_info = json.loads(user_response.text)
+
+    for user in user_info:
+        user_id = user['id']
+
+        # create Response object for specific user and that user's tasks
+        url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
+            .format(user_id)
+        todo_response = requests.get(url)
+
+        # create Dictionary objects from response objects
+        todo_info = json.loads(todo_response.text)
+
+        tasks = {}
+
+        tasks_list = []
+        for task in todo_info:
+            # create a dictionary for each task
+            task_dict = {}
+            task_dict['username'] = user['username']
+            task_dict['task'] = task['title']
+            task_dict['completed'] = task['completed']
+            # add task dictionary to the list of tasks
+            tasks_list.append(task_dict)
+
+        # add list of tasks to dictionary
+        tasks[user_id] = tasks_list
+
+    with open('todo_all_employees.json', 'w', encoding='UTF8',
+              newline='') as f:
+        f.write(json.dumps(tasks))
