6ca14265a28de31bb71d141fa8366e15e32189e2
mdallen5393 <allen_matthew@outlook.com>
2022-11-30 10:20:51 -0800

    Task 0. Gather data from an API

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -7,30 +7,33 @@ import json
 import requests
 import sys
 
-num_done, num_tasks = 0, 0
-userId = sys.argv[1]
+if __name__ == "__main__":
+    num_done, num_tasks = 0, 0
+    userId = sys.argv[1]
 
-# create Response object for specific user and that user's tasks
-user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                             .format(userId))
-todo_response = requests \
-    .get('https://jsonplaceholder.typicode.com/todos/?userId={}'
-         .format(userId))
+    # create Response object for specific user and that user's tasks
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                                .format(userId))
+    todo_response = requests \
+        .get('https://jsonplaceholder.typicode.com/todos/?userId={}'
+            .format(userId))
 
-# create Dictionary objects from response objects
-user_info = json.loads(user_response.text)
-todo_info = json.loads(todo_response.text)
+    # create Dictionary objects from response objects
+    user_info = json.loads(user_response.text)
+    todo_info = json.loads(todo_response.text)
+    print(type(user_info))
+    print(type(todo_info))
 
-employee_name = user_info['name']
+    employee_name = user_info['name']
 
-for task in todo_info:
-    num_tasks += 1
-    if task['completed']:
-        num_done += 1
+    for task in todo_info:
+        num_tasks += 1
+        if task['completed']:
+            num_done += 1
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(employee_name, num_done, num_tasks))
+    print("Employee {} is done with tasks({}/{}):"
+        .format(employee_name, num_done, num_tasks))
 
-for task in todo_info:
-    if task['completed']:
-        print("\t {}".format(task['title']))
+    for task in todo_info:
+        if task['completed']:
+            print("\t {}".format(task['title']))
